04ac912498315e713de8a5d13834cd7210abf9ba
Caleb Denio <cjdenio44@gmail.com>
2022-05-14 01:56:59 -0400

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,11 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: vercel({
+			edge: false,
+			external: [],
+			split: true
+		})
 	}
 };
 
